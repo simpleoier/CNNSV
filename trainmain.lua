0c778c9caf45ca27115893167401fd91d9351358
Xuank <netnetchangxk@gmail.com>
2015-07-27 10:30:54 +0800

    add removing last two layers

--- trainmain.lua
+++ trainmain.lua
@@ -1,5 +1,3 @@
--- require 'torch'
-----------------------------------------------------------------------
 print '==> executing all'
 
 dofile 'init.lua'
@@ -14,14 +12,30 @@ print '==> training!'
 
 local trainfbankfilelist = opt.scpfile
 local listfile = io.open(trainfbankfilelist, 'r')
-while (true) do
-    trainData = ReadData(listfile)
-    if (trainData:size()>0) then
-        shuffle = torch.randperm(trainData:size())
-        train()
-    else
-        break
-    end
-    collectgarbage()
-end
-listfile:close()
+-- while (true) do
+--     trainData = ReadData(listfile)
+--     if (trainData:size()>0) then
+--         shuffle = torch.randperm(trainData:size())
+--         train()
+--     else
+--         break
+--     end
+--     collectgarbage()
+-- end
+-- listfile:close()
+
+-- print(model:size())
+-- trainData = ReadData(listfile)
+-- for i=1,3 do
+--     shuffle = torch.randperm(trainData:size())
+--     train()
+--     print(parameters:size())
+--     print(parameters[3],parameters[30],parameters[300],parameters[3000],parameters[30000])
+--     --print(model:get(1).weight:size())
+--     print(model:get(1).weight[1][3],model:get(1).weight[1][30],model:get(1).weight[1][300],model:get(1).weight[3][360],model:get(1).weight[23][960])
+--     parameters[3] = 100
+--     print(parameters[3],parameters[30],parameters[300],parameters[3000],parameters[30000])
+--     --print(model:get(1).weight:size())
+--     print(model:get(1).weight[1][3],model:get(1).weight[1][30],model:get(1).weight[1][300],model:get(1).weight[3][360],model:get(1).weight[23][960])
+-- end
+-- listfile:close()
